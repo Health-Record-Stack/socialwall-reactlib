a1aa29d40106116d00050f965f4e45297d842b58
Sanish Joseph <sanyjose85@gmail.com>
2020-04-21 19:29:08 -0700

    Published workable solution
    Removed React-Bootstrap to avoid publish error, refactored code

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 import { Socialwall } from 'socialwall-reactlib'
 import 'socialwall-reactlib/dist/index.css'
 
 const App = () => {
-  return <Socialwall />
+  return (
+    <Container>
+      <Row>
+        <Col md="3">--Left Content--</Col>
+        <Col md="6">
+          <Socialwall />
+        </Col>
+        <Col md="3">--Right Content--</Col>
+      </Row>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap'
 import fetchSocualwallSeed from './services/socialwall.service'
 import SocialwallItem from './components/SocialwallItem'
-// import styles from './styles.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export const Socialwall = () => {
   const [socialwallItems, setSocialwallItems] = useState([])
   // eslint-disable-next-line no-unused-vars
   const [limit, setLimit] = useState(10)
   const [skip, setSkip] = useState(0)
+  const [facebookScriptLoaded, setFacebookScriptLoaded] = useState(false)
+  const [twitterScriptLoaded, setTwitterScriptLoaded] = useState(false)
 
   const handleFetchMore = () => {
     setSkip(skip + 10)
@@ -20,39 +21,50 @@ export const Socialwall = () => {
     const twitterScript = 'https://platform.twitter.com/widgets.js'
     const script = require('scriptjs')
     script(fbScript, 'facebook-embed', () => {
-      script(twitterScript, 'twitter-embed', () => {
-        async function fetchData(limit, skip) {
-          const res = await fetchSocualwallSeed(limit, skip)
-          const apiResponse = await res.json()
-          if (apiResponse.status === 1) {
-            setSocialwallItems([...socialwallItems, ...apiResponse.data])
-            window.twttr.widgets.load()
-            window.FB.init({
-              appId: 'your-app-id',
-              autoLogAppEvents: true,
-              xfbml: true,
-              version: 'v6.0'
-            })
-          } else console.log(apiResponse)
-        }
-        fetchData(limit, skip)
-      })
+      setFacebookScriptLoaded(true)
+      console.log('Facebook scripts loaded')
     })
-  }, [skip])
+    script(twitterScript, 'twitter-embed', () => {
+      setTwitterScriptLoaded(true)
+      console.log('Twitter scripts loaded')
+    })
+  }, [])
+
+  useEffect(() => {
+    async function fetchData(limit, skip) {
+      const res = await fetchSocualwallSeed(limit, skip)
+      const apiResponse = await res.json()
+      if (apiResponse.status === 1) {
+        setSocialwallItems([...socialwallItems, ...apiResponse.data])
+        window.twttr.widgets.load()
+        window.FB.init({
+          appId: '1670671936512008',
+          autoLogAppEvents: true,
+          xfbml: true,
+          version: 'v6.0'
+        })
+      } else console.log(apiResponse)
+    }
+    if (facebookScriptLoaded && twitterScriptLoaded) fetchData(limit, skip)
+  }, [skip, facebookScriptLoaded, twitterScriptLoaded])
 
   return (
-    <Container fluid='md'>
-      <Row>
-        <Col>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
           {socialwallItems.map((item) => (
             <SocialwallItem key={item._id} item={item} />
           ))}
           {/* <SocialwallList feed={s}></SocialwallList> */}
-        </Col>
-        <Col>
-          <Button onClick={handleFetchMore}>Fetch More...</Button>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+      <div className='row mt-2 mb-2'>
+        <div className='col text-center'>
+          <button onClick={handleFetchMore} className='btn btn-success'>
+            Load More...
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
